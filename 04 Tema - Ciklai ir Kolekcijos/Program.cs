53b19d105e8021d669f51251ec2ebc5b8c037198
4Karolis <4karolis.t@gmail.com>
2022-03-21 21:57:22 +0200

    Adden UZDUOTIS + TEORIJA List'ai

--- 04 Tema - Ciklai ir Kolekcijos/Program.cs
+++ 04 Tema - Ciklai ir Kolekcijos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04_Tema___Ciklai_ir_Kolekcijos
 {
@@ -382,64 +383,341 @@ namespace _04_Tema___Ciklai_ir_Kolekcijos
             //}
             //-------------------------------------------------------------------------------------------------------------------------------
 
-            #region KOLEKCIJOS
+            #region KOLEKCIJOS TEORIJA
 
-            string[] savaitesDienos =
+            //string[] savaitesDienos =
+            //{
+            //    "Pirmadienis",
+            //    "Antradienis",
+            //    "Treciadienis",
+            //    "Ketvirtadienis",
+            //    "Penbnktadienis",
+            //    "Sestadienis",
+            //    "Sekmadienis",
+            //};
+            //for (int i = 0; i < savaitesDienos.Length; i++)
+            //{
+            //    Console.WriteLine(savaitesDienos[i]);
+            //}
+
+            //int[] integers = { 1, 5, 6 };
+
+            //string pirmadienis = savaitesDienos[0];
+            //Console.WriteLine("Kuria savaites diena parodyti?");
+            //Console.WriteLine("Pirmadienis  - 1 ir t.t.");
+
+            //int index = int.Parse(Console.ReadLine());
+            //string selectedDay = savaitesDienos[index - 1];
+
+            //Console.WriteLine($"Buvo pasirinkta savaites diena:{selectedDay} ");
+
+            //// Elemento keitimas masyve 
+            //Console.WriteLine("Pries:");
+
+            //foreach (var diena in savaitesDienos)
+            //{
+            //    Console.WriteLine(diena);
+            //}
+
+            //savaitesDienos[2] = "TRECIADIENIS";
+
+            //Console.WriteLine("Po: ");
+            //foreach(var diena in savaitesDienos)
+            //{
+            //    Console.WriteLine(diena);
+            //}
+
+            //// Masyvo uzpildymas random skaiciais
+            //int[] skaiciai  = new int[10];
+            //string[] t = new string[10];
+
+            //Random random = new Random();
+
+            //for (int i = 0; i < skaiciai.Length; i++)
+            //{
+            //    skaiciai[i] = random.Next(1, 100);// generuo 10 random skaiciu, verte nuo 1 iki 100
+            //}
+            //foreach (var skaicius in skaiciai)
+            //{
+            //    Console.WriteLine(skaicius);
+            //}
+
+
+
+            ////MSYVO DYZDIO KEITIMAS
+
+            //char[] zodis = new char[6];
+            //zodis[0] = 'K';
+            //zodis[1] = 'u';
+            //zodis[2] = 'r';
+            //zodis[3] = 's';
+            //zodis[4] = 'a';
+            //zodis[5] = 's';
+
+            //foreach (var raide in zodis)
+            //{
+            //    Console.WriteLine(raide);
+            //}
+
+            //Array.Resize(ref zodis, 8);
+
+            //zodis[6] = 'C';
+            //zodis[7] = '#';
+
+            //foreach (var raide in zodis)
+            //{
+            //    Console.WriteLine(raide);
+            //}
+            #endregion
+
+            #region UZDUOYS 
+            //------------------------------------------------ 1 UZDUOTIS ----------------------------------------------------------
+
+
+            //Console.WriteLine("Iveskite pirma skaiciu: ");
+            //int num1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Iveskite antra skaiciu: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Iveskite trecia skaiciu: ");
+            //int num3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Iveskite ketvirta skaiciu: ");
+            //int num4 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Iveskite penkta skaiciu: ");
+            //int num5 = int.Parse(Console.ReadLine());
+
+
+            //int[] skaiciai = new int[5];
+            //skaiciai[0] = num1;
+            //skaiciai[1] = num2;
+            //skaiciai[2] = num3;
+            //skaiciai[3] = num4;
+            //skaiciai[4] = num5;
+
+            //int[] temp = skaiciai;
+
+            //Console.WriteLine("Jusu ivesti skaiciai yra: ");
+            //foreach (var skaicius in skaiciai)
+            //{
+            //    Console.WriteLine(skaicius);
+            //}
+
+            //Array.Reverse(temp);
+            //Console.WriteLine("Jusu ivesti skaiciai atvirkstine tvarka: ");
+            //foreach (var skaicius2 in temp)
+            //{
+            //    Console.WriteLine(skaicius2);
+            //}
+
+
+            //////-------------------------------------------------- 2 UZDUOTIS ----------------------------------------------------
+
+            //int[] arr = skaiciai;
+            //int suma = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    suma += arr[i];
+            //}
+            //Console.WriteLine($"Jusu ivestu skaiciu suma: {suma}");
+
+            ////--------------------------------------------------- 3 UZDUOTIS ----------------------------------------------------
+
+            //Console.WriteLine("Iveskite pirma skaiciu: ");
+            //int input1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Iveskite antra skaiciu: ");
+            //int input2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Iveskite trecia skaiciu: ");
+            //int input3 = int.Parse(Console.ReadLine());
+
+            //int[] inputai = new int[3] {input1,input2,input3};
+            //Console.WriteLine($"Siuo metu masyve esantys skaiciai: ");
+
+            //foreach (var inputas in inputai)
+            //{
+            //    Console.WriteLine(inputas);
+            //}
+
+            //Console.WriteLine("Iveskite ketvirta skaiciu: ");
+            //int input4 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Iveskite penkta skaiciu: ");
+            //int input5 = int.Parse(Console.ReadLine());
+
+            //Array.Resize(ref inputai, 5);
+            //inputai[3] = input4;
+            //inputai[4] = input5;
+
+            //Console.WriteLine("Dabar masyve esantys skaiciai: ");
+            //foreach (var inputas in inputai)
+            //{
+            //    Console.WriteLine(inputas);
+            //}
+
+            //------------------------------------------------------ 4 UZDUOTIS ---------------------------------------------------
+
+            //int[] skaiciai = new int[10];
+
+            //Random random = new Random();
+
+            //for (int i = 0; i < skaiciai.Length; i++)
+            //{
+            //    skaiciai[i] = random.Next(1, 100);// generuo 10 random skaiciu, verte nuo 1 iki 100
+            //}
+
+            //Console.WriteLine("Masyve esantys atsitiktiniai skaiciai: ");
+            //foreach (var skaicius in skaiciai)
+            //{
+            //    Console.WriteLine(skaicius);
+            //}
+
+            //Array.Sort(skaiciai);
+            ////Console.WriteLine("Masyvo skaiciai didejancia tvarka: ");
+            ////foreach (var skaicius in skaiciai)
+            ////{
+            ////    Console.WriteLine(skaicius);
+            ////}
+            //Console.WriteLine($"Maziausias skaicius masyve: {skaiciai[0]}");
+            //Console.WriteLine($"Didziausias skaicius masyve: {skaiciai[9]}");
+
+            //Nesukciaujant:
+
+
+
+            //--------------------------------------------------- 5 UZDUOTIS ------------------------------------------------------
+
+            //// 1 random array
+            //int[] randomSkaiciai = new int[5];
+            //Random random1 = new Random();
+            //Console.WriteLine("PIRMAS random array: ");
+            //for (int i = 0; i < randomSkaiciai.Length; i++)
+            //{
+            //    randomSkaiciai[i] = random1.Next(1, 47);
+            //}
+            //foreach (var randomSkaicius in randomSkaiciai)
+            //{
+            //    Console.WriteLine(randomSkaicius);
+            //}
+
+            //// 2 random array
+            //int[] randomSkaiciai2 = new int[5];
+            //Random random2 = new Random();
+            //Console.WriteLine("ANTRAS random array: ");
+            //for (int i = 0; i < randomSkaiciai2.Length; i++)
+            //{
+            //    randomSkaiciai2[i] = random2.Next(1, 47);
+            //}
+            //foreach (var randomSkaicius2 in randomSkaiciai2)
+            //{
+            //    Console.WriteLine(randomSkaicius2);
+            //}
+
+            //// Sujungtas array
+
+            ////int[] sujungti = randomSkaiciai.Concat(randomSkaiciai2).ToArray();
+            //int[] sujungti = new int[randomSkaiciai.Length + randomSkaiciai2.Length]; // nurodom kokio dyzdio bus array
+            //randomSkaiciai.CopyTo(sujungti, 0); // 0 rodo nuo kurio indekso kopinam
+            //randomSkaiciai2.CopyTo(sujungti, randomSkaiciai.Length); // tesim kopinima nuo to, kiek buvo random pirmam.
+
+            //Console.WriteLine("Sujungti Array: ");
+            //foreach (var item in sujungti)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //for (int i = 0; i < sujungti.Length; i++)// i = i+1 -- AREJAUS SORTINIMAS
+            //{
+            //    for (int j = i + 1; j < sujungti.Length; j++)// j = j  +1
+            //    {
+            //        if(sujungti[i] > sujungti[j]) //< daro nuo didziausio iki maziausio - padarius > duos nuo maziausio iki didziausio
+            //        {
+            //            int temp = sujungti[i];
+            //            sujungti[i] = sujungti[j];
+            //            sujungti[j] = temp;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Isrykiavus");
+
+            //foreach (var item in sujungti)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //--------------------------------------------------- 6 uzduotis --------------------------------------------------------
+
+            char[] zodis = new char[6];
+
+            zodis[0] = 'K';
+            zodis[1] = 'u';
+            zodis[2] = 'r';
+            zodis[3] = 's';
+            zodis[4] = 'a';
+            zodis[5] = 's';
+
+            Console.WriteLine("Array versija pries isimant simboli: ");
+            Console.WriteLine(zodis);
+
+            char[] naujasZodis = new char[zodis.Length - 1];
+
+            int m = 0;
+            int n = 0;
+            int removeAt = 4; // irasom kuri index is array isimsim
+
+            while (m < zodis.Length)
             {
-                "Pirmadienis",
-                "Antradienis",
-                "Treciadienis",
-                "Ketvirtadienis",
-                "Penbnktadienis",
-                "Sestadienis",
-                "Sekmadienis",
-            };
-            for (int i = 0; i < savaitesDienos.Length; i++)
-            {
-                Console.WriteLine(savaitesDienos[i]);
+                if (m != removeAt)
+                {
+                    naujasZodis[n] = zodis[m];
+                    n++;
+                }
+                m++;
             }
 
-            int[] integers = { 1, 5, 6 };
+            Console.WriteLine("\nNaujas isvestas zodis: ");
+            Console.WriteLine(naujasZodis); // isvedam gautas zodi
 
-            string pirmadienis = savaitesDienos[0];
-            Console.WriteLine("Kuria savaites diena parodyti?");
-            Console.WriteLine("Pirmadienis  - 1 ir t.t.");
+            #endregion
 
-            int index = int.Parse(Console.ReadLine());
-            string selectedDay = savaitesDienos[index - 1];
+            #region LIST<T> TEORIJA
 
-            Console.WriteLine($"Buvo pasirinkta savaites diena:{selectedDay} ");
+            List<string> listas = new List<string>();
+            //Listo pildymas
+            listas.Add("Jonas");
+            listas.Add("Petras");
+            listas.Add("Juozas");
 
-            // Elemento keitimas masyve 
-            Console.WriteLine("Pries:");
+            //Kitas budas papildyt lista
+            List<string> listas2 = new List<string> { "Jonas", "Petras", "Juozas" };
 
-            foreach (var diena in savaitesDienos)
+            //isvedimui naudoja FOREACH arba FOR (naudojamt COUNT vietoj .Lenght)
+            for (int i = 0; i < listas.Count; i++)
             {
-                Console.WriteLine(diena);
+                Console.WriteLine(listas[i]);
+            }
+            foreach (var item in listas)
+            {
+                Console.WriteLine(item);
             }
 
-            savaitesDienos[2] = "TRECIADIENIS";
+            listas.RemoveAt(5);//removina 5 itema
+            listas.Insert(5, "Onute");
+            listas.AddRange(listas2); // concatina abu listus.
+            bool isContains = listas.Contains("Petras");
+            //listas.REadonly nebeleidzia pridet nieko
+            int indexas = listas.LastIndexOf("Patras");
 
-            Console.WriteLine("Po: ");
-            foreach(var diena in savaitesDienos)
-            {
-                Console.WriteLine(diena);
-            }
+            List<int> intList = new List<int>();
+            List<bool> boolList = new List<bool>();
+            List<object> objektlistas = new List<object>();//priima bet kokius tipus, bet uzsiknisi isimant info is ten
 
-            // Masyvo uzpildymas random skaiciais
-            int[] skaiciai  = new int[10];
-            string[] t = new string[10];
+            objektlistas.Add(true);
+            objektlistas.Add("true");
 
-            Random random = new Random();
-
-            for (int i = 0; i < skaiciai.Length; i++)
-            {
-                skaiciai[i] = random.Next(1, 100);// generuo 10 random skaiciu, verte nuo 1 iki 100
-            }
-            foreach (var skaicius in skaiciai)
-            {
-                Console.WriteLine(skaicius);
-            }
+            boolList.Add(true);
 
             #endregion
         }
